e4ce77a913f80efbce73b6893db0f52c3cf7f8e2
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-05-26 13:06:08 +0430

    data added

---
+++ src/data.js
@@ -0,0 +1,1010 @@
+const PEOPLE = [
+        {
+            id: 0,
+            name: 'Aria',
+            sex: 'girl'
+        }, {
+            id: 1,
+            name: 'Logan',
+            sex: 'boy'
+        }, {
+            id: 2,
+            name: 'Quinn',
+            sex: 'girl'
+        }, {
+            id: 3,
+            name: 'Kai',
+            sex: 'boy'
+        }, {
+            id: 4,
+            name: 'Dashiell',
+            sex: 'boy'
+        }, {
+            id: 5,
+            name: 'John',
+            sex: 'boy'
+        }, {
+            id: 6,
+            name: 'Seraphina',
+            sex: 'girl'
+        }, {
+            id: 7,
+            name: 'Caroline',
+            sex: 'girl'
+        }, {
+            id: 8,
+            name: 'Tobias',
+            sex: 'boy'
+        }, {
+            id: 9,
+            name: 'Harper',
+            sex: 'girl'
+        }, {
+            id: 10,
+            name: 'Mabe',
+            sex: 'girl'
+        }, {
+            id: 11,
+            name: 'Iris',
+            sex: 'girl'
+        }, {
+            id: 12,
+            name: 'Beatrice',
+            sex: 'girl'
+        }, {
+            id: 13,
+            name: 'Knox',
+            sex: 'boy'
+        }, {
+            id: 14,
+            name: 'August',
+            sex: 'boy'
+        }, {
+            id: 15,
+            name: 'Poppy',
+            sex: 'girl'
+        }, {
+            id: 16,
+            name: 'Aurora',
+            sex: 'girl'
+        }, {
+            id: 17,
+            name: 'Wyatt',
+            sex: 'boy'
+        }, {
+            id: 18,
+            name: 'Ezra',
+            sex: 'boy'
+        }, {
+            id: 19,
+            name: 'Emily',
+            sex: 'girl'
+        }, {
+            id: 20,
+            name: 'Sebastian',
+            sex: 'boy'
+        },
+        {
+            id: 21,
+            name: 'Gabriel',
+            sex: 'boy'
+        },
+        {
+            id: 22,
+            name: 'Isabella',
+            sex: 'girl'
+        },
+        {
+            id: 23,
+            name: 'Elliot',
+            sex: 'boy'
+        },
+        {
+            id: 24,
+            name: 'Benjamin',
+            sex: 'boy'
+        },
+        {
+            id: 25,
+            name: 'Declan',
+            sex: 'boy'
+        },
+        {
+            id: 26,
+            name: 'Locas',
+            sex: 'boy'
+        },
+        {
+            id: 27,
+            name: 'Gemma',
+            sex: 'girl'
+        },
+        {
+            id: 28,
+            name: 'Chloe',
+            sex: 'girl'
+        
+        },
+        {
+            id: 29,
+            name: 'Khaleesi',
+            sex: 'girl'
+        
+        },
+        {
+            id: 30,
+            name: 'Matilda',
+            sex: 'girl'
+        
+        },
+        {
+            id: 31,
+            name: 'Grace',
+            sex: 'girl'
+        },
+        {
+            id: 32,
+            name: 'Grayson',
+            sex: 'boy'
+        },
+        {
+            id: 33,
+            name: 'Beckett',
+            sex: 'boy'
+        },
+        {
+            id: 34,
+            name: 'Nicholas',
+            sex: 'boy'
+        },
+        {
+            id: 35,
+            name: 'Ronan',
+            sex: 'boy'
+        },
+        {
+            id: 36,
+            name: 'Nathaniel',
+            sex: 'boy'
+        },
+        {
+            id: 37,
+            name: 'Ryker',
+            sex: 'boy'
+        },
+        {
+            id: 38,
+            name: 'Eliza',
+            sex: 'girl'
+        },
+        {
+            id: 39,
+            name: 'Imogen',
+            sex: 'girl'
+        },
+        {
+            id: 40,
+            name: 'Amelia',
+            sex: 'girl'
+        },
+        {
+            id: 41,
+            name: 'Olivia',
+            sex: 'girl'
+        },
+        {
+            id: 42,
+            name: 'Clara',
+            sex: 'girl'
+        },
+        {
+            id: 43,
+            name: 'Lily',
+            sex: 'girl'
+        
+        },
+        {
+            id: 44,
+            name: 'Sloane',
+            sex: 'girl'
+        
+        },
+        {
+            id: 45,
+            name: 'Zoe',
+            sex: 'girl'
+        
+        },
+        {
+            id: 46,
+            name: 'Penelope',
+            sex: 'girl'
+        
+        },
+        {
+            id: 47,
+            name: 'Evangeline',
+            sex: 'girl'
+        },
+        {
+            id: 48,
+            name: 'Julia',
+            sex: 'girl'
+        },
+        {
+            id: 49,
+            name: 'Juliet',
+            sex: 'girl'
+        
+        },
+        {
+            id: 50,
+            name: 'Leo',
+            sex: 'boy'
+        
+        },
+        {
+            id: 51,
+            name: 'Zane',
+            sex: 'boy'
+        
+        },
+        {
+            id: 52,
+            name: 'Owen',
+            sex: 'boy'
+        
+        },
+        {
+            id: 53,
+            name: 'Julian',
+            sex: 'boy'
+        },
+        {
+            id: 54,
+            name: 'Caleb',
+            sex: 'boy'
+        },
+        {
+            id: 55,
+            name: 'Nolan',
+            sex: 'boy'
+        },
+        {
+            id: 56,
+            name: 'Roman',
+            sex: 'boy'
+        },
+        {
+            id: 57,
+            name: 'Fleix',
+            sex: 'boy'
+        },
+        {
+            id: 58,
+            name: 'Aryan',
+            sex: 'boy'
+        },
+        {
+            id: 59,
+            name: 'Theo',
+            sex: 'boy'
+        },
+        {
+            id: 60,
+            name: 'Nathan',
+            sex: 'boy'
+        },
+        {
+            id: 61,
+            name: 'Josiah',
+            sex: 'boy'
+        },
+        {
+            id: 62,
+            name: 'Harrison',
+            sex: 'boy'
+        },
+        {
+            id: 63,
+            name: 'Archer',
+            sex: 'boy'
+        },
+        {
+            id: 64,
+            name: 'Rowan',
+            sex: 'boy'
+        },
+        {
+            id: 65,
+            name: 'Rhys',
+            sex: 'boy'
+        },
+        {
+            id: 66,
+            name: 'Emmentt',
+            sex: 'boy'
+        },
+        {
+            id: 67,
+            name: 'Elle',
+            sex: 'girl'
+        },
+        {
+            id: 68,
+            name: 'Eden',
+            sex: 'girl'
+        },
+        {
+            id: 69,
+            name: 'Evelyn',
+            sex: 'girl'
+        },
+        {
+            id: 70,
+            name: 'Isabel',
+            sex: 'girl'
+        },
+        {
+            id: 71,
+            name: 'Mae',
+            sex: 'girl'
+        },
+        {
+            id: 72,
+            name: 'Mila',
+            sex: 'girl'
+        },
+        {
+            id: 73,
+            name: 'Rose',
+            sex: 'girl'
+        },
+        {
+            id: 74,
+            name: 'Willow',
+            sex: 'girl'
+        },
+        {
+            id: 75,
+            name: 'Cora',
+            sex: 'girl'
+        },
+        {
+            id: 76,
+            name: 'Axel',
+            sex: 'boy'
+        },
+        {
+            id: 77,
+            name: 'David',
+            sex: 'boy'
+        },
+        {
+            id: 78,
+            name: 'Miles',
+            sex: 'boy'
+        },
+        {
+            id: 79,
+            name: 'Oscar',
+            sex: 'boy'
+        },
+        {
+            id: 80,
+            name: 'Silas',
+            sex: 'boy'
+        },
+        {
+            id: 81,
+            name: 'Clementine',
+            sex: 'girl'
+        },
+        {
+            id: 82,
+            name: 'Sophia',
+            sex: 'girl'
+        },
+        {
+            id: 83,
+            name: 'Eloise',
+            sex: 'girl'
+        },
+        {
+            id: 84,
+            name: 'Everly',
+            sex: 'girl'
+        },
+        {
+            id: 85,
+            name: 'Maeve',
+            sex: 'girl'
+        },
+        {
+            id: 86,
+            name: 'Nora',
+            sex: 'girl'
+        },
+        {
+            id: 87,
+            name: 'Lucy',
+            sex: 'girl'
+        },
+        {
+            id: 88,
+            name: 'Adeline',
+            sex: 'girl'
+        },
+        {
+            id: 89,
+            name: 'Hazel',
+            sex: 'girl'
+        },
+        {
+            id: 90,
+            name: 'Ruby',
+            sex: 'girl'
+        },
+        {
+            id: 91,
+            name: 'Jude',
+            sex: 'boy'
+        },
+        {
+            id: 92,
+            name: 'Lachlan',
+            sex: 'boy'
+        },
+        {
+            id: 93,
+            name: 'Hugo',
+            sex: 'boy'
+        },
+        {
+            id: 94,
+            name: 'Maddox',
+            sex: 'boy'
+        },
+        {
+            id: 95,
+            name: 'Asher',
+            sex: 'boy'
+        },
+        {
+            id: 96,
+            name: 'Jayden',
+            sex: 'boy'
+        
+        },
+        {
+            id: 97,
+            name: 'Callum',
+            sex: 'boy'
+        
+        },
+        {
+            id: 98,
+            name: 'Jasper',
+            sex: 'boy'
+        },
+        {
+            id: 100,
+            name: 'Mia',
+            sex: 'girl'
+        },
+        {
+            id: 101,
+            name: 'Evie',
+            sex: 'girl'
+        },
+        {
+            id: 102,
+            name: 'Luna',
+            sex: 'girl'
+        },
+        {
+            id: 103,
+            name: 'Wern',
+            sex: 'girl'
+        },
+        {
+            id: 104,
+            name: 'Hadley',
+            sex: 'girl'
+        },
+        {
+            id: 105,
+            name: 'Ivy',
+            sex: 'girl'
+        },
+        {
+            id: 106,
+            name: 'Emmeline',
+            sex: 'girl'
+        },
+        {
+            id: 107,
+            name: 'Stella',
+            sex: 'girl'
+        },
+        {
+            id: 108,
+            name: 'Daisy',
+            sex: 'girl'
+        
+        },
+        {
+            id: 109,
+            name: 'Emma',
+            sex: 'girl'
+        },
+        {
+            id: 110,
+            name: 'Jonah',
+            sex: 'boy'
+        },
+        {
+            id: 111,
+            name: 'Sawyer',
+            sex: 'boy'
+        },
+        {
+            id: 112,
+            name: 'Soren',
+            sex: 'boy'
+        },
+        {
+            id: 113,
+            name: 'Beau',
+            sex: 'boy'
+        },
+        {
+            id: 114,
+            name: 'Maxwell',
+            sex: 'boy'
+        },
+        {
+            id: 115,
+            name: 'James',
+            sex: 'boy'
+        },
+        {
+            id: 116,
+            name: 'Claire',
+            sex: 'girl'
+        
+        },
+        {
+            id: 117,
+            name: 'Lola',
+            sex: 'girl'
+        
+        },
+        {
+            id: 118,
+            name: 'Sienna',
+            sex: 'girl'
+        
+        },
+        {
+            id: 119,
+            name: 'Ada',
+            sex: 'girl'
+        
+        },
+        {
+            id: 120,
+            name: 'Scarlett',
+            sex: 'girl'
+        
+        },
+        {
+            id: 121,
+            name: 'Hannah',
+            sex: 'girl'
+        
+        },
+        {
+            id: 122,
+            name: 'Maya',
+            sex: 'girl'
+       
+        },
+        {
+            id: 123,
+            name: 'Arabella',
+            sex: 'girl'
+       
+        },
+        {
+            id: 124,
+            name: 'Anna',
+            sex: 'girl'
+       
+        },
+        {
+            id: 125,
+            name: 'Lila',
+            sex: 'girl'
+       
+        },
+        {
+            id: 126,
+            name: 'Sadie',
+            sex: 'girl'
+       
+        },
+        {
+            id: 127,
+            name: 'Arthur',
+            sex: 'boy'
+        },
+        {
+            id: 128,
+            name: 'Samuel',
+            sex: 'boy'
+        },
+        {
+            id: 129,
+            name: 'Atticus',
+            sex: 'boy'
+        },
+        {
+            id: 130,
+            name: 'Sadie',
+            sex: 'boy'
+        },
+        {
+            id: 131,
+            name: 'Bodhi',
+            sex: 'boy'
+        },
+        {
+            id: 132,
+            name: 'Elijah',
+            sex: 'boy'
+        },
+        {
+            id: 133,
+            name: 'Griffin',
+            sex: 'boy'
+        },
+        {
+            id: 134,
+            name: 'Graham',
+            sex: 'boy'
+        },
+        {
+            id: 135,
+            name: 'Weston',
+            sex: 'boy'
+        },
+        {
+            id: 136,
+            name: 'Hudson',
+            sex: 'boy'
+        },
+        {
+            id: 137,
+            name: 'Henry',
+            sex: 'boy'
+        },
+        {
+            id: 138,
+            name: 'Levi',
+            sex: 'boy'
+        },
+        {
+            id: 139,
+            name: 'Aurelia',
+            sex: 'girl'
+        },
+        {
+            id: 140,
+            name: 'Nova',
+            sex: 'girl'
+        },
+        {
+            id: 141,
+            name: 'Margaret',
+            sex: 'girl'
+        },
+        {
+            id: 142,
+            name: 'Adelaide',
+            sex: 'girl'
+        },
+        {
+            id: 143,
+            name: 'Audrey',
+            sex: 'girl'
+        },
+        {
+            id: 144,
+            name: 'Jane',
+            sex: 'girl'
+        },
+        {
+            id: 145,
+            name: 'Alice',
+            sex: 'girl'
+        },
+        {
+            id: 146,
+            name: 'simon',
+            sex: 'boy'
+        },
+        {
+            id: 147,
+            name: 'Thomas',
+            sex: 'boy'
+        },
+        {
+            id: 148,
+            name: 'Oliver',
+            sex: 'boy'
+        },
+        {
+            id: 149,
+            name: 'Ethan',
+            sex: 'boy'
+        },
+        {
+            id: 150,
+            name: 'Liam',
+            sex: 'boy'
+        },
+        {
+            id: 151,
+            name: 'Alexander',
+            sex: 'boy'
+        },
+        {
+            id: 152,
+            name: 'Wesley',
+            sex: 'boy'
+        },
+        {
+            id: 153,
+            name: 'William',
+            sex: 'boy'
+        },
+        {
+            id: 154,
+            name: 'Issac',
+            sex: 'boy'
+        },
+        {
+            id: 155,
+            name: 'Ella',
+            sex: 'girl'
+        },
+        {
+            id: 156,
+            name: 'Violet',
+            sex: 'girl'
+        },
+        {
+            id: 157,
+            name: 'Madeline',
+            sex: 'girl'
+        },
+        {
+            id: 158,
+            name: 'Genevieve',
+            sex: 'girl'
+        },
+        {
+            id: 159,
+            name: 'Lydia',
+            sex: 'girl'
+        },
+        {
+            id: 160,
+            name: 'Harlow',
+            sex: 'girl'
+        },
+        {
+            id: 161,
+            name: 'Eliana',
+            sex: 'girl'
+        },
+        {
+            id: 162,
+            name: 'Esme',
+            sex: 'girl'
+        },
+        {
+            id: 163,
+            name: 'Elizabeth',
+            sex: 'girl'
+        },
+        {
+            id: 164,
+            name: 'Xavier',
+            sex: 'boy'
+        },
+        {
+            id: 165,
+            name: 'Andrew',
+            sex: 'boy'
+        },
+        {
+            id: 166,
+            name: 'Everett',
+            sex: 'boy'
+        },
+        {
+            id: 167,
+            name: 'Lincoln',
+            sex: 'boy'
+        },
+        {
+            id: 168,
+            name: 'Eli',
+            sex: 'boy'
+        },
+        {
+            id: 169,
+            name: 'Piper',
+            sex: 'girl'
+        },
+        {
+            id: 170,
+            name: 'Abigail',
+            sex: 'girl'
+        },
+        {
+            id: 171,
+            name: 'Olive',
+            sex: 'girl'
+        },
+        {
+            id: 172,
+            name: 'Elodie',
+            sex: 'girl'
+        },
+        {
+            id: 173,
+            name: 'Josephine',
+            sex: 'girl'
+        },
+        {
+            id: 174,
+            name: 'Thea',
+            sex: 'girl'
+        },
+        {
+            id: 175,
+            name: 'Emilia',
+            sex: 'girl'
+        },
+        {
+            id: 176,
+            name: 'Florence',
+            sex: 'girl'
+        },
+        {
+            id: 177,
+            name: 'Ava',
+            sex: 'girl'
+        },
+        {
+            id: 178,
+            name: 'Isla',
+            sex: 'girl'
+        },
+        {
+            id: 179,
+            name: 'Willa',
+            sex: 'girl'
+        },
+        {
+            id: 180,
+            name: 'Charlotte',
+            sex: 'girl'
+        },
+        {
+            id: 181,
+            name: 'Cordelia',
+            sex: 'girl'
+        },
+        {
+            id: 182,
+            name: 'Eleanor',
+            sex: 'girl'
+        },
+        {
+            id: 183,
+            name: 'Annabelle',
+            sex: 'girl'
+        },
+        {
+            id: 184,
+            name: 'Noah',
+            sex: 'boy'
+        },
+        {
+            id: 185,
+            name: 'Matthew',
+            sex: 'boy'
+        },
+        {
+            id: 186,
+            name: 'Luca',
+            sex: 'boy'
+        },
+        {
+            id: 187,
+            name: 'Finn',
+            sex: 'boy'
+        },
+        {
+            id: 188,
+            name: 'Theodore',
+            sex: 'boy'
+        },
+        {
+            id: 189,
+            name: 'George',
+            sex: 'boy'
+        },
+        {
+            id: 190,
+            name: 'Jackson',
+            sex: 'boy'
+        },
+        {
+            id: 191,
+            name: 'Jacob',
+            sex: 'boy'
+        },
+        {
+            id: 192,
+            name: 'Daniel',
+            sex: 'boy'
+        },
+        {
+            id: 193,
+            name: 'Ryder',
+            sex: 'boy'
+        },
+        {
+            id: 194,
+            name: 'Zachary',
+            sex: 'boy'
+        },
+        {
+            id: 195,
+            name: 'Luke',
+            sex: 'boy'
+        },
+        {
+            id: 196,
+            name: 'Jack',
+            sex: 'boy'
+        },
+        {
+            id: 197,
+            name: 'Charles',
+            sex: 'boy'
+        },
+        {
+            id: 198,
+            name: 'Bennett',
+            sex: 'boy'
+        },
+        {
+            id: 199,
+            name: 'Nicolas',
+            sex: 'boy'
+        },
+        {
+            id: 200,
+            name: 'Damon',
+            sex: 'boy'
+        }
+        
+]
+
+export default PEOPLE
