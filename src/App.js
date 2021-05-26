f0928517945dfaa51ad73c1402624ad9688f3dd3
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-05-26 15:12:36 +0430

    adding NamesList component to the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React, { useState } from 'react';
+import NamesList from './component/NamesList/NamesList';
+import PEOPLE from './data'
+import './App.scss';
 
 function App() {
+
   return (
     <div className="App">
-      
+      <NamesList data={PEOPLE}/>
     </div>
   );
 }
