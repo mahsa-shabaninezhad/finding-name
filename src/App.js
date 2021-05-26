9277aed809182eb6bac21d0d9055ff08c40ccc32
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-05-26 15:37:08 +0430

    adding Button to the app for clearing input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import NamesList from './component/NamesList/NamesList';
 import Search from './component/Search/Search';
+import Button from './component/Button/Button';
 import PEOPLE from './data'
 import './App.scss';
 
@@ -10,10 +11,16 @@ function App() {
     setSearch(search)
   }
 
+  const handleClearInput = () =>{
+    setSearch('')
+  }
+
   return (
     <div className="App">
       <Search onSearch={handleSearch} search={search}/>
       <NamesList data={PEOPLE} search={search}/>
+      {/*button for clearing input*/}
+      {search && <Button content='Clear List' onClick={handleClearInput}/>}
     </div>
   );
 }
