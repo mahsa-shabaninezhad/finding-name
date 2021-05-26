cf27a05211a32da7a9ed36f58d87f363fec19f99
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-05-26 15:28:53 +0430

    adding Search component to the App and defining search state and handlSearch due to creating control component in Search and using search in NamesList component for filtering names

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import NamesList from './component/NamesList/NamesList';
+import Search from './component/Search/Search';
 import PEOPLE from './data'
 import './App.scss';
 
 function App() {
+  const [search, setSearch] = useState('')
+  const handleSearch = (search) =>{
+    setSearch(search)
+  }
 
   return (
     <div className="App">
-      <NamesList data={PEOPLE}/>
+      <Search onSearch={handleSearch} search={search}/>
+      <NamesList data={PEOPLE} search={search}/>
     </div>
   );
 }
